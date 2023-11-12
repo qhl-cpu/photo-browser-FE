c2279e072e5ec38cf10ab41c12e4fe8d7e155b2d
Davis Qi <907225883wxl@gmail.com>
2023-11-12 15:11:51 -0800

    simplify photos.map rendering with grid

--- src/components/PhotoGenerator.js
+++ src/components/PhotoGenerator.js
@@ -72,25 +72,14 @@ const PhotoGenerator = () => {
 
   return (
     <div className='flex flex-col justify-center items-center h-screen'>
-      <div className='flex overflow-x-auto p-4 space-x-4 bg-gray-300 max-w-[80%]'>
+      <div className='grid grid-flow-col grid-rows-2 grid-cols-none overflow-x-scroll overflow-y-hidden rounded-xl bg-gray-300 p-4 max-w-[80%]'>
         {photos.map((photo, index) => {
-          if (index % 2 === 0) { // Only render for even indices (0, 2, 4, ...)
-            return (
-              <div className='flex-shrink-0'
-                key={"photoMap" + photo.id} >
-                <Tooltip id={photo.id} place='top'/>
-                <PhotoItem photo={ photo }/>
-
-                {index + 1 < photos.length && (
-                  <>
-                    <Tooltip id={photos[index + 1].id} place='bottom'/>
-                    <PhotoItem photo={ photos[index + 1] } />
-                  </>
-                )}
-              </div>
-            )
-          }
-          return null;
+          return (
+            <div className='w-32 y-32 mx-2' key={"photoMap" + photo.id} >
+              <Tooltip id={ photo.id } place={ index % 2 == 0 ? 'top' : 'bottom' }/>
+              <PhotoItem photo={ photo }/>
+            </div>
+          )
         })}
       </div>
       <button className='mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
