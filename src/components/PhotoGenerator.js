b947382634415f4adf4706ff367469e4c09bd695
Davis Qi <907225883wxl@gmail.com>
2023-11-11 17:44:44 -0800

    created photo generator component with shuffle/regenerate btn

---
+++ src/components/PhotoGenerator.js
@@ -0,0 +1,15 @@
+const PhotoGenerator = () => {
+
+    return (
+        <div>
+            <button>
+                Shuffle Photos
+            </button>
+            <button>
+                Generate New Photos
+            </button>
+        </div>
+    );
+}
+
+export default PhotoGenerator;
