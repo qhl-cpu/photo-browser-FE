334f97d66b8bff1e10892c1521bc2edcd0636f49
Davis Qi <907225883wxl@gmail.com>
2023-11-11 17:53:58 -0800

    install tailwind

--- src/components/PhotoGenerator.js
+++ src/components/PhotoGenerator.js
@@ -2,7 +2,7 @@ const PhotoGenerator = () => {
 
     return (
         <div>
-            <button>
+            <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 Shuffle Photos
             </button>
             <button>
