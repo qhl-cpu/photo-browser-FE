84189bb17802ec6d763a2e913eb977e5f9074a68
Davis Qi <907225883wxl@gmail.com>
2023-11-11 17:58:07 -0800

    install eslint

--- src/components/PhotoGenerator.js
+++ src/components/PhotoGenerator.js
@@ -1,15 +1,15 @@
 const PhotoGenerator = () => {
 
-    return (
-        <div>
-            <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Shuffle Photos
-            </button>
-            <button>
-                Generate New Photos
-            </button>
-        </div>
-    );
+  return (
+    <div>
+      <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Shuffle Photos
+      </button>
+      <button>
+            Generate New Photos
+      </button>
+    </div>
+  );
 }
 
 export default PhotoGenerator;
